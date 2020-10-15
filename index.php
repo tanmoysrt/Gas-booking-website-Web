0af2c233534d33c8dc18c68894239d7a3c11f870
Tanmoy Sarkar <57363826+Tanmoy741127@users.noreply.github.com>
2020-10-15 23:02:37 +0530

    update

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 <body style='background: linear-gradient(45deg, rgba(34,193,195,1) 0%, rgba(241,6,20,1) 0%, rgba(252,47,108,1) 100%);'>
    <img src="man.svg" alt="" height="400px" width="375px"> 
     
-   <div class="card" style="text-align: center;width: 100%;padding-top: 10px;padding-bottom: 10px">
+   <div style="text-align: center;width: 100%;height:300px;padding-top: 10px;padding-bottom: 10px">
     <form action="/verify.php" method="GET" >
         <input type="text" style="border-radius: 30px; border: 1px solid red;height: 30px;width: 300px;text-align: center;font-family: 'Piazzolla', serif;" name="number" placeholder='Enter Phone No' ><br>
         <button class="btn btn-warning" style="padding: 10px"><a style="padding: 5px">Submit</a></button>
