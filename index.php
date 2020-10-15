a614cd7a911ed6bee6539b6ff0eb726ff0051de8
Tanmoy Sarkar <57363826+Tanmoy741127@users.noreply.github.com>
2020-10-15 22:58:19 +0530

    bs4

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="https://fonts.googleapis.com/css2?family=Piazzolla:wght@500&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/css/bootstrap.min.css" integrity="sha384-DhY6onE6f3zzKbjUPRc2hOzGAdEf4/Dz+WJwBvEYL/lkkIsI3ihufq9hk9K4lVoK" crossorigin="anonymous">
 </head>
 <body style='background: linear-gradient(45deg, rgba(34,193,195,1) 0%, rgba(241,6,20,1) 0%, rgba(252,47,108,1) 100%);'>
    <img src="man.svg" alt="" height="400px" width="375px"> 
@@ -18,6 +17,6 @@
     </div>
 
 </body>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/js/bootstrap.bundle.min.js" integrity="sha384-BOsAfwzjNJHrJ8cZidOg56tcQWfp6y72vEJ8xQ9w6Quywb24iOsW913URv1IS4GD" crossorigin="anonymous"></script>
+
     
 </html>
