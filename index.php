1fd17e1f4ee1fe10335871faffee753cf1a12267
Tanmoy Sarkar <57363826+Tanmoy741127@users.noreply.github.com>
2020-10-16 16:27:10 +0530

    Update index.php

--- index.php
+++ index.php
@@ -9,9 +9,9 @@
 <body style='background: linear-gradient(45deg, rgba(34,193,195,1) 0%, rgba(241,6,20,1) 0%, rgba(252,47,108,1) 100%);'>
    <img src="man.svg" alt="" height="400px" width="375px"> 
     
-   <div style="text-align: center;width: 100%;height:300px;padding-top: 10px;padding-bottom: 10px">
+   <div style="text-align: center;width: 100%;height:500px;padding-top: 10px;padding-bottom: 10px">
     <form action="/verify.php" method="GET" >
-        <input type="text" style="border-radius: 30px; border: 1px solid red;height: 30px;width: 500px;text-align: center;font-family: 'Piazzolla', serif;" name="number" placeholder='Enter Phone No' required ><br>
+        <input type="text" style="border-radius: 30px; border: 1px solid red;height: 30px;width: 300px;text-align: center;font-family: 'Piazzolla', serif;" name="number" placeholder='Enter Phone No' required ><br>
         <button class="btn btn-warning" style="padding: 10px"><a style="padding: 5px">Submit</a></button>
     </form>
     </div>
